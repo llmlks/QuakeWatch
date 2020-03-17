f8708f859c2d43f8d9d41777a3b2efa51e5a64a3
llmlks <lamelkas@gmail.com>
2020-03-17 12:06:45 +0200

    Handle invalid minutes

--- utils/dateutils.py
+++ utils/dateutils.py
@@ -6,7 +6,7 @@ def get_datetime(year, month, day, hour, minute, second_float):
     """Return datetime object with given arguments. If argument second
     contains a value larger than or equal to 60, convert it to a valid
     value by taking its modulo against 60 and adding one minute to the
-    datetime.
+    datetime. Handle invalid minute values (>= 60) similarly.
 
     Keyword arguments:
     year -- Year as an integer
@@ -26,6 +26,14 @@ def get_datetime(year, month, day, hour, minute, second_float):
             year, month, day, hour, minute, second, microsecond
         )
         date = date + timedelta(minutes=1)
+        return date
+
+    if minute >= 60:
+        minute = minute % 60
+        date = datetime(
+            year, month, day, hour, minute, second, microsecond
+        )
+        date = date + timedelta(hours=1)
         return date
 
     return datetime(
