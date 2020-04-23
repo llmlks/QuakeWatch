48e2e62834921321f2d69e86eb9f9f6a8c6bffa2
llmlks <lamelkas@gmail.com>
2020-04-23 13:34:57 +0300

    Show cumulative eqs with log-time opacity

--- components/config/uncertainty_toggler.py
+++ components/config/uncertainty_toggler.py
@@ -2,15 +2,14 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
-DEFAULT_TIMESTEP = 24*60*60
-
 
 def get_component():
-    """Return the timestep picker component."""
+    """Return the uncertainty toggler component."""
 
     return html.Div(children=[
         dbc.Checklist(
             id='uncertainty-toggle',
+            className='toggle-select',
             options=[{
                 'label': 'Visualize location uncertainties',
                 'value': 1
